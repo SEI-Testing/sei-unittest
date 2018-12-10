92853d3b4001a1aca32c605631d762f5c6d00afd
RBLU <reto.blunschi@youpers.com>
2018-12-10 14:21:08 +0100

    add circleci config

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,21 +3,14 @@ const fizzbuzz = require('../src/fizzbuzz');
 describe('FizzBuzz', function() {
 
     it('bei 0 soll 0 zurückkommen', function() {
-        expect(fizzbuzz(0)).toBe(1);
-        expect(fizzbuzz(3)).toBe('fizz');
+        expect(fizzbuzz(0)).toBe(0);
+        expect(fizzbuzz(6)).toBe('fizz');
 
-        expect(fizzbuzz(5)).toBe('buzz');
-
-        expect(fizzbuzz(0)).toBe(1);
-
-        expect(fizzbuzz(0)).toBe(1);
-
-        expect(fizzbuzz(0)).toBe(1);
+        expect(fizzbuzz(10)).toBe('buzz');
 
     });
 
-    it('true should be true', function() {
-    })
+
 
 
 });
