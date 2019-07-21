9726e746fd54a5d3cb19b688f04eb7a7049a8c1f
RBLU <reto.blunschi@youpers.com>
2019-07-21 21:44:38 +0200

    fix example

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,15 +3,19 @@ const fizzbuzz = require('../src/fizzbuzz');
 describe('FizzBuzz', function() {
 
     it('bei 0 soll 0 zurückkommen', function() {
-        expect(fizzbuzz(0)).toBe(1);
-        expect(fizzbuzz(6)).toBe('fizz');
+        expect(fizzbuzz(0)).toBe(0);
+    });
 
-        expect(fizzbuzz(10)).toBe('buzz');
+    it('bei 6 soll fizz zurückkommen', function() {
+        expect(fizzbuzz(6)).toBe('fizz');
 
     });
 
+    it('bei 10 soll buzz zurückkommen', function() {
 
+        expect(fizzbuzz(11)).toBe('buzz');
 
+    });
 
 });
 
